fcf923f44d414cc9458abe624f7067ffb5a3e4a3
ddomhall <domjshall@gmail.com>
2024-04-20 09:58:41 +0100

    3.1

---
+++ 2015/03/1.py
@@ -0,0 +1,19 @@
+text = open('input.txt').read()
+
+coords = [0, 0]
+visited = set([f'{coords[0]}, {coords[1]}'])
+
+for c in text:
+    match c:
+        case '^':
+            coords[1] += 1
+        case '>':
+            coords[0] += 1
+        case 'v':
+            coords[1] -= 1
+        case '<':
+            coords[0] -= 1
+    visited.add(f'{coords[0]}, {coords[1]}')
+
+print(len(visited))
+
