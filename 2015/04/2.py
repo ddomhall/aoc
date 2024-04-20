169ce80e00f3203502caa24dc7ed5bf474cf019a
ddomhall <domjshall@gmail.com>
2024-04-20 10:24:42 +0100

    4.2

--- 2015/04/2.py
+++ 2015/04/2.py
@@ -4,8 +4,9 @@ text = open('input.txt').read().strip()
 
 num = 0
 while True:
-    if hashlib.md5((text + str(num)).encode()).hexdigest().startswith('00000'):
+    if hashlib.md5((text + str(num)).encode()).hexdigest().startswith('000000'):
         break
     num += 1
 
 print(num)
+
