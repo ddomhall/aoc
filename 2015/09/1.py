974db9a1cd1b173968e4d4d2a7f4dfcda594ffad
ddomhall <domjshall@gmail.com>
2024-04-25 05:49:45 +0100

    9.1 better structure

--- 2015/09/1.py
+++ 2015/09/1.py
@@ -1,7 +1,21 @@
 text = open('input.txt').read().strip().split('\n')
+text = open('test_input.txt').read().strip().split('\n')
 
-routes = []
+routes = {}
 for line in text:
     line = line.split()
-    routes.append({'route': [line[0], line[2]], 'distance': line[4] })
+    try:
+        routes[line[0]].append([ line[2], int(line[4]) ])
+    except:
+        routes[line[0]] = [[ line[2], int(line[4]) ]]
+    try:
+        routes[line[2]].append([ line[0], int(line[4]) ])
+    except:
+        routes[line[2]] = [[ line[0], int(line[4]) ]]
+
+''' {
+    'London': [['Dublin', 464], ['Belfast', 518]],
+    'Dublin': [['London', 464], ['Belfast', 141]],
+    'Belfast': [['London', 518], ['Dublin', 141]]
+} '''
 
