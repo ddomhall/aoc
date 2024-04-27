cb894a156d138871a442c5df3282a468cc586879
ddomhall <domjshall@gmail.com>
2024-04-27 08:35:04 +0100

    12.1

---
+++ 2015/12/1.py
@@ -0,0 +1,20 @@
+import json
+input = json.load(open('input.json'))
+
+total = 0
+def search(jsn):
+    if type(jsn) == int:
+        global total
+        total += jsn
+        return
+    elif type(jsn) == list:
+        for item in jsn:
+            search(item)
+    elif type(jsn) == dict:
+        for item in jsn.values():
+            search(item)
+    else:
+        return
+
+search(input)
+print(total)
