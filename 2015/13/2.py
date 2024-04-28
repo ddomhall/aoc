ed2492f5644963088a21d57bc74c4b0a428147fb
ddomhall <domjshall@gmail.com>
2024-04-28 04:48:22 +0100

    13.2

---
+++ 2015/13/2.py
@@ -0,0 +1,25 @@
+from itertools import permutations 
+input = open('input.txt').read().splitlines()
+
+seats = dict()
+for row in input:
+    user1, _, diff, value, _, _, _, _, _, _, user2 = row.rstrip('.').split()
+    seats.setdefault(user1, dict())[user2] = int(value) if diff == 'gain' else - int(value)
+    seats.setdefault(user1, dict())['me'] = 0
+    seats.setdefault('me', dict())[user1] = 0
+
+happiness = 0
+for pattern in permutations(seats):
+    total = 0
+    for i in range(len(pattern)):
+        name = pattern[i]
+        if i == 0:
+            total += seats[name][pattern[i + 1]] + seats[name][pattern[-1]]
+        elif i == len(pattern) - 1:
+            total += seats[name][pattern[0]] + seats[name][pattern[i - 1]]
+        else:
+            total += seats[name][pattern[i + 1]] + seats[name][pattern[i - 1]]
+    if total > happiness: happiness = total
+
+print(happiness)
+
