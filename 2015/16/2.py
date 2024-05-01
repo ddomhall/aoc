66de3e2465ab73e13d8a5b09b6fae6ea5095de90
ddomhall <domjshall@gmail.com>
2024-05-01 05:12:32 +0100

    16.2

---
+++ 2015/16/2.py
@@ -0,0 +1,27 @@
+input = [{ 'number': int(x[1].rstrip(':')), x[2].rstrip(':'): int(x[3].rstrip(',')), x[4].rstrip(':'): int(x[5].rstrip(',')), x[6].rstrip(':'): int(x[7].rstrip(',')) } for x in [x.split() for x in open('input.txt').read().splitlines()]]
+
+ref = {
+    'children': 3,
+    'cats': 7,
+    'samoyeds': 2,
+    'pomeranians': 3,
+    'akitas': 0,
+    'vizslas': 0,
+    'goldfish': 5,
+    'trees': 3,
+    'cars': 2,
+    'perfumes': 1
+}
+
+for sue in input:
+    count = 0
+    for prop in sue:
+        if prop == 'number': continue
+        elif prop in ['cats', 'trees']:
+            if sue[prop] > ref[prop]: count += 1
+        elif prop in ['pomeranians', 'goldfish']:
+            if sue[prop] < ref[prop]: count += 1
+        else:
+            if sue[prop] == ref[prop]: count += 1
+    if count == 3: print(sue)
+
