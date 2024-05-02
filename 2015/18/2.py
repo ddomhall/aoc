ba258bdf3772c9cf619831b4c09b3efff7d4979c
ddomhall <domjshall@gmail.com>
2024-05-02 05:36:17 +0100

    18.2

---
+++ 2015/18/2.py
@@ -0,0 +1,36 @@
+from copy import deepcopy
+input = [list(x) for x in open('input.txt').read().splitlines()]
+input[0][0] = input[0][-1] = input[-1][0] = input[-1][-1] = '#'
+
+def get_neighbours(x, y, copy):
+    if x == 0 and y == 0:
+        return 3
+    elif x == len(copy[0]) - 1 and y == 0:
+        return 3
+    elif x == 0 and y == len(copy) - 1:
+        return 3
+    elif x == len(copy[0]) - 1 and y == len(copy) - 1:
+        return 3
+    elif y == 0:
+        neighbours = copy[y][x-1] + copy[y][x+1] + copy[y+1][x-1] + copy[y+1][x] + copy[y+1][x+1]
+    elif x == 0:
+        neighbours = copy[y-1][x] + copy[y-1][x+1] + copy[y][x+1] + copy[y+1][x] + copy[y+1][x+1]
+    elif y == len(copy) - 1:
+        neighbours = copy[y-1][x-1] + copy[y-1][x] + copy[y-1][x+1] + copy[y][x-1] + copy[y][x+1]
+    elif x == len(copy[0]) - 1:
+        neighbours = copy[y-1][x-1] + copy[y-1][x] + copy[y][x-1] + copy[y+1][x-1] + copy[y+1][x]
+    else:
+        neighbours = copy[y-1][x-1] + copy[y-1][x] + copy[y-1][x+1] + copy[y][x-1] + copy[y][x+1] + copy[y+1][x-1] + copy[y+1][x] + copy[y+1][x+1]
+    return neighbours.count('#')
+
+for step in range(100):
+    ref = deepcopy(input)
+    for y in range(len(input)):
+        for x in range(len(input)):
+            input[y][x] = '#' if (input[y][x] == '#' and get_neighbours(x, y, ref) in [2, 3]) or (input[y][x] == '.' and get_neighbours(x, y, ref) == 3) else '.'
+
+count = 0
+for row in input:
+    count += row.count('#')
+print(count)
+
