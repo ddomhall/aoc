61f17ee66defdcdf2f59d8bdc603e87bdc7146cb
ddomhall <domjshall@gmail.com>
2024-05-02 05:22:21 +0100

    18.1

--- 2015/18/1.py
+++ 2015/18/1.py
@@ -1,23 +1,34 @@
+from copy import deepcopy
 input = [list(x) for x in open('input.txt').read().splitlines()]
-input = [list(x) for x in open('test_input.txt').read().splitlines()]
-print(input)
 
 def get_neighbours(x, y, copy):
-    return 0
+    if x == 0 and y == 0:
+        neighbours = copy[y][x+1] + copy[y+1][x] + copy[y+1][x+1]
+    elif x == len(copy[0]) - 1 and y == 0:
+        neighbours = copy[y][x-1] + copy[y+1][x-1] + copy[y+1][x]
+    elif x == 0 and y == len(copy) - 1:
+        neighbours = copy[y-1][x] + copy[y-1][x+1] + copy[y][x+1]
+    elif x == len(copy[0]) - 1 and y == len(copy) - 1:
+        neighbours = copy[y-1][x-1] + copy[y-1][x] + copy[y][x-1]
+    elif y == 0:
+        neighbours = copy[y][x-1] + copy[y][x+1] + copy[y+1][x-1] + copy[y+1][x] + copy[y+1][x+1]
+    elif x == 0:
+        neighbours = copy[y-1][x] + copy[y-1][x+1] + copy[y][x+1] + copy[y+1][x] + copy[y+1][x+1]
+    elif y == len(copy) - 1:
+        neighbours = copy[y-1][x-1] + copy[y-1][x] + copy[y-1][x+1] + copy[y][x-1] + copy[y][x+1]
+    elif x == len(copy[0]) - 1:
+        neighbours = copy[y-1][x-1] + copy[y-1][x] + copy[y][x-1] + copy[y+1][x-1] + copy[y+1][x]
+    else:
+        neighbours = copy[y-1][x-1] + copy[y-1][x] + copy[y-1][x+1] + copy[y][x-1] + copy[y][x+1] + copy[y+1][x-1] + copy[y+1][x] + copy[y+1][x+1]
+    return neighbours.count('#')
 
-for step in range(1):
-    ref = input.copy()
+for step in range(100):
+    ref = deepcopy(input)
     for y in range(len(input)):
         for x in range(len(input)):
-            if input[y][x] == '#':
-                if get_neighbours(x, y, ref) in [2, 3]:
-                    input[y][x] = '#'
-                else:
-                    input[y][x] = '.'
-            else:
-                if get_neighbours(x, y, ref) == 3:
-                    input[y][x] = '#'
-                else:
-                    input[y][x] = '.'
+            input[y][x] = '#' if (input[y][x] == '#' and get_neighbours(x, y, ref) in [2, 3]) or (input[y][x] == '.' and get_neighbours(x, y, ref) == 3) else '.'
 
-print(input)
+count = 0
+for row in input:
+    count += row.count('#')
+print(count)
