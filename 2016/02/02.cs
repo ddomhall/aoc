b8a585edab1a3e59273ccb6d7bdf6636bfbc44ad
ddomhall <domjshall@gmail.com>
2024-07-20 14:16:16 +0100

    2.2

--- 2016/02/02.cs
+++ 2016/02/02.cs
@@ -2,11 +2,11 @@ public class Day2
 {
 	static string[] input = File.ReadAllText("./02/input.txt").Trim().Split();
 	// static string[] input = File.ReadAllText("./02/test-input.txt").Trim().Split();
-	static int[][] keypad = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-	static int[] coords = new int[] { 1, 1 };
 
 	public static void Part1()
 	{
+		int[][] keypad = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+		int[] coords = new int[] { 1, 1 };
 		Console.Write("2.1: ");
 		foreach (string line in input)
 		{
@@ -29,6 +29,38 @@ public class Day2
 				}
 			}
 			Console.Write(keypad[coords[0]][coords[1]]);
+		}
+		Console.WriteLine();
+	}
+
+	public static void Part2()
+	{
+		int?[][] keypad = [[null, null, 1, null, null], [null, 2, 3, 4, null], [5, 6, 7, 8, 9], [null, 'A', 'B', 'C', null], [null, null, 'D', null, null]];
+		int[] coords = new int[] { 2, 0 };
+		Console.Write("2.2: ");
+		foreach (string line in input)
+		{
+			foreach (char direction in line.ToCharArray())
+			{
+				switch (direction)
+				{
+					case 'U':
+						if (coords[0] > 0 && keypad[coords[0] - 1][coords[1]] != null) coords[0]--;
+						break;
+					case 'D':
+						if (coords[0] < 4 && keypad[coords[0] + 1][coords[1]] != null) coords[0]++;
+						break;
+					case 'L':
+						if (coords[1] > 0 && keypad[coords[0]][coords[1] - 1] != null) coords[1]--;
+						break;
+					case 'R':
+						if (coords[1] < 4 && keypad[coords[0]][coords[1] + 1] != null) coords[1]++;
+						break;
+				}
+			}
+			int? key = keypad[coords[0]][coords[1]];
+			if (key > 9) Console.Write(Convert.ToChar(key));
+			else Console.Write(key);
 		}
 		Console.WriteLine();
 	}
--- 2016/Program.cs
+++ 2016/Program.cs
@@ -1,5 +1,7 @@
-﻿/*
+﻿Console.Clear();
+/*
 Day1.Part1();
 Day1.Part2();
-*/
 Day2.Part1();
+*/
+Day2.Part2();
