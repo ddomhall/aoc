37e0791685521b59ff4808ff2b38d4091997bb39
ddomhall <domjshall@gmail.com>
2024-08-26 20:05:47 +0100

    3 refactored

--- 2016/03/03.cs
+++ 2016/03/03.cs
@@ -2,35 +2,36 @@ public class Day3
 {
 	static string[] triangles = File.ReadAllText("./03/input.txt").Trim().Split("\n");
 	// static string[] triangles = File.ReadAllText("./03/test-input.txt").Trim().Split("\n");
-	static int count = 0;
+	static int count;
 
 	public static void Part1()
 	{
+		count = 0;
 		foreach (string line in triangles)
 		{
-			int[] t = line.Trim().Split().Where(x => !string.IsNullOrEmpty(x)).Select(Int32.Parse).ToArray();
-			if (t[0] + t[1] > t[2] && t[0] + t[2] > t[1] && t[1] + t[2] > t[0]) count++;
+			if (ValidTriangle(SplitAndConvert(line))) count++;
 		}
 		Console.WriteLine(count);
 	}
 
 	public static void Part2()
 	{
+		count = 0;
 		List<int> row0 = new List<int>();
 		List<int> row1 = new List<int>();
 		List<int> row2 = new List<int>();
 
 		for (int row = 0; row < triangles.Count(); row++)
 		{
-			int[] t = triangles[row].Trim().Split().Where(x => !string.IsNullOrEmpty(x)).Select(Int32.Parse).ToArray();
+			List<int> t = SplitAndConvert(triangles[row]);
 			row0.Add(t[0]);
 			row1.Add(t[1]);
 			row2.Add(t[2]);
 			if (row2.Count() == 3)
 			{
-				if (row0[0] + row0[1] > row0[2] && row0[0] + row0[2] > row0[1] && row0[1] + row0[2] > row0[0]) count++;
-				if (row1[0] + row1[1] > row1[2] && row1[0] + row1[2] > row1[1] && row1[1] + row1[2] > row1[0]) count++;
-				if (row2[0] + row2[1] > row2[2] && row2[0] + row2[2] > row2[1] && row2[1] + row2[2] > row2[0]) count++;
+				if (ValidTriangle(row0)) count++;
+				if (ValidTriangle(row1)) count++;
+				if (ValidTriangle(row2)) count++;
 				row0.Clear();
 				row1.Clear();
 				row2.Clear();
@@ -38,4 +39,7 @@ public class Day3
 		}
 		Console.WriteLine(count);
 	}
+
+	static List<int> SplitAndConvert(string input) => input.Trim().Split().Where(x => !string.IsNullOrEmpty(x)).Select(Int32.Parse).ToList();
+	static bool ValidTriangle(List<int> input) => (input[0] + input[1] > input[2] && input[0] + input[2] > input[1] && input[1] + input[2] > input[0]);
 }
--- 2016/Program.cs
+++ 2016/Program.cs
@@ -4,6 +4,6 @@ Day1.Part1();
 Day1.Part2();
 Day2.Part1();
 Day2.Part2();
-Day3.Part1();
 */
+Day3.Part1();
 Day3.Part2();
