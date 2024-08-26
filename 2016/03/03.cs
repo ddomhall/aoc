36874dfba3d50e492982c9b948b3de237fcd435c
ddomhall <domjshall@gmail.com>
2024-08-26 18:58:36 +0100

    3.1 refactored

--- 2016/03/03.cs
+++ 2016/03/03.cs
@@ -2,13 +2,12 @@ public class Day3
 {
 	public static void Part1()
 	{
-		string[] triangles = File.ReadAllText("./03/input.txt").Split("\n");
+		string[] triangles = File.ReadAllText("./03/input.txt").Trim().Split("\n");
 		int count = 0;
 		foreach (string line in triangles)
 		{
-			if (string.IsNullOrEmpty(line)) continue;
-			string[] t = line.Trim().Split().Where(x => !string.IsNullOrEmpty(x)).ToArray();
-			if (Int32.Parse(t[0]) + Int32.Parse(t[1]) > Int32.Parse(t[2]) && Int32.Parse(t[0]) + Int32.Parse(t[2]) > Int32.Parse(t[1]) && Int32.Parse(t[1]) + Int32.Parse(t[2]) > Int32.Parse(t[0])) count++;
+			int[] t = line.Trim().Split().Where(x => !string.IsNullOrEmpty(x)).Select(Int32.Parse).ToArray();
+			if (t[0] + t[1] > t[2] && t[0] + t[2] > t[1] && t[1] + t[2] > t[0]) count++;
 		}
 		Console.WriteLine(count);
 	}
